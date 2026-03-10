4e483c41d084e413f76f3b356ab7a864f5ca9d4a
Quynh Nhu <nguyennuquynhnhu13@gmail.com>
2026-03-10 23:49:13 +0700

    sync jira 5s

--- -swp391-project-management-tool/src/main/java/com/qnhu/swp391projectmanagementtool/Swp391ProjectManagementToolApplication.java
+++ -swp391-project-management-tool/src/main/java/com/qnhu/swp391projectmanagementtool/Swp391ProjectManagementToolApplication.java
@@ -2,8 +2,10 @@ package com.qnhu.swp391projectmanagementtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Swp391ProjectManagementToolApplication {
 
     public static void main(String[] args) {
